2de83841ac1435a35c05c770e0c0238a12feefc7
Kristján Jakob Ásgrímsson <kristjanjakobasgrimsson@askja-dw406.rhi.hi.is>
2026-04-03 16:20:23 +0000

    Laga smá villur

--- lib/datocms.ts
+++ lib/datocms.ts
@@ -1,68 +1,3 @@
-export type Homepage = {
-    title: string;
-    intro: string;
-};
-  
-export type Post = {
-    title: string;
-    slug: string;
-    excerpt: string;
-};
-  
-export type PostDetail = {
-    title: string;
-    slug: string;
-    content: string;
-};
-const API_URL = 'https://graphql.datocms.com/';
-const API_TOKEN = process.env.DATOCMS_API_TOKEN!;
-
-async function fetchAPI(query: string, variables?: Record<string, unknown>) {
-  const res = await fetch(API_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${API_TOKEN}`,
-    },
-    body: JSON.stringify({ query, variables }),
-    next: { revalidate: 60 },
-  });
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data from DatoCMS');
-  }
-
-  const json = await res.json();
-  return json.data;
-}
-
-export async function getHomepage() {
-  const query = `
-    query HomepageQuery {
-      homepage {
-        title
-        intro
-      }
-    }
-  `;
-  const data = await fetchAPI(query);
-  return data.homepage;
-}
-
-export async function getPosts() {
-  const query = `
-    query PostsQuery {
-      allPosts(orderBy: publishedAt_DESC) {
-        title
-        slug
-        excerpt
-      }
-    }
-  `;
-  const data = await fetchAPI(query);
-  return data.allPosts;
-}
-
 import { executeQuery } from '@datocms/cda-client';
 
 export type Homepage = {
@@ -85,7 +20,7 @@ export type PostDetail = {
 export async function performRequest<T>(
   query: string,
   variables?: Record<string, unknown>
-) {
+): Promise<T> {
   return executeQuery<T>(query, {
     token: process.env.DATOCMS_API_TOKEN!,
     variables,
@@ -109,7 +44,7 @@ export async function getHomepage(): Promise<Homepage> {
 export async function getPosts(): Promise<Post[]> {
   const query = `
     query PostsQuery {
-      allPosts(orderBy: publishedAt_DESC) {
+      allPosts {
         title
         slug
         excerpt
